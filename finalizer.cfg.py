5a0b14a7b2422ade3370dc157e1d7b8995f78e6f
escs system manager <manager@escsMaster.med.ira.inaf.it>
2017-06-19 11:26:26 +0200

    Modified according to Medicina tests

--- finalizer.cfg.py
+++ finalizer.cfg.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 job_files_incoming_folder = '/archive/report/'
-job_files_done_folder = '/archive/done_report/'
-tar_folder = '/archive/tars'
+job_files_done_folder = '/archive/report.old/'
+tar_folder = '/locallustre/finalized_data'
 
-job_status_folder = '/archive/finalizer_status' 
+job_status_folder = '/home/manager/.finalizer/finalizer_status' 
 lock_file = '/archive/locks/ScheduleRecording.lck'
-pid_file = '/tmp/finalizer.pid'
+pid_file = '/home/manager/.finalizer/finalizer.pid'
 
 # 1: errori non gestiti, 2: errori gestiti, 3: info
 debug_level = 3
